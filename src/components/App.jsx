2a10fc88b3c797a060637b5601a55e065387c8c2
DanyilUA <122489621+DanyilUA@users.noreply.github.com>
2023-07-31 18:46:37 +0200

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,34 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from "react";
+import SearchBar from "./Searchbar/Searchbar";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { ImageGallery } from "./ImageGallery/ImageGallery";
+
+
+
+
+export class App extends Component {
+
+  state = {
+    imageName: '',
+    loading: false,
+    currentPageNumber: 1,
+
+  };
+
+
+  handleFormSubmit = imageName => {
+    this.setState({ imageName, currentPageNumber: 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <SearchBar onSubmit={this.handleFormSubmit} />
+        <ImageGallery imageName={this.state.imageName} />
+        <ToastContainer autoClose={3000} />
+      </div>
+    );
+  }
+
 };
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import css from './Button.module.css';
+
+
+export function Button(props) {
+  console.log(props)
+  return (
+    <div className={css.buttonWrapper}>
+      <button type="button" className={css.button} onClick={props.onClick}>
+        Load more
+      </button>
+    </div>
+  );
+};
